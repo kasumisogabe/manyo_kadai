014d1335f1c557a5ea4382b8bb7828acdbe34f86
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-01 17:17:52 +0900

    CRUD機能作成

---
+++ db/migrate/20230301064638_create_tasks.rb
@@ -0,0 +1,9 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :title, :string, null: false
+    change_column :tasks, :content, :text, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230301070350_change_not_null_to_tasks.rb
@@ -0,0 +1,4 @@
+class ChangeNotNullToTasks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
