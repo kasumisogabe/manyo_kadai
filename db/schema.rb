473db2618362b766ac66b08db5bdd1ded140cb33
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-02 17:48:54 +0900

    不要なテーブルを削除したため

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_071431) do
+ActiveRecord::Schema.define(version: 2023_03_02_081533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "controllers", force: :cascade do |t|
-    t.string "Task"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
