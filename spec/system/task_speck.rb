fff8b936a8fbb4616be3e6e9bfb35bcb695428fc
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-06 16:45:55 +0900

    タスク一覧を作成日時の順番で並び替え、テスト実装

--- spec/system/task_speck.rb
+++ spec/system/task_speck.rb
@@ -20,7 +20,19 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task'
       end
     end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task1 = FactoryBot.create(:task, title: 'task1', content: 'content1', created_at: Time.zone.now)
+        task2 = FactoryBot.create(:task, title: 'task2', content: 'content2', created_at: Time.zone.now - 1.day)
+        visit tasks_path
+        task_list = all('.task_list') 
+        expect(task_list[0]).to have_content 'task1'
+        expect(task_list[1]).to have_content 'task2'
+      end
+    end
   end
+
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
