7e1d3d1091470a05df949df9ae5a0b6ce7355529
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-10 13:46:30 +0900

    UserとTaskのアソシエーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  has_many :tasks
+  
   has_secure_password
 
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: { minimum: 6 }
-  
+
   before_validation { email.downcase! }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_020528) do
+ActiveRecord::Schema.define(version: 2023_03_10_044013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2023_03_10_020528) do
     t.date "limit"
     t.string "status"
     t.string "priority"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
